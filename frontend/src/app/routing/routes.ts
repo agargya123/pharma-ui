efea441ec88fc5060c5a04a9c60c863cd13032b9
Arunav Sharma <arunavsharma11@gmail.com>
2020-05-20 23:50:22 +0530

    Basic Timeline added

--- frontend/src/app/routing/routes.ts
+++ frontend/src/app/routing/routes.ts
@@ -2,13 +2,13 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "../login/login.component";
 import { HomeComponent } from "../home/home.component";
 import { DashboardComponent } from "../dashboard/dashboard.component";
-import {FetchSaltsComponent} from "../fetch-salts/fetch-salts.component";
+import { FetchSaltsComponent} from "../fetch-salts/fetch-salts.component";
 import { ManufactureDrugsComponent } from '../manufacture-drugs/manufacture-drugs.component';
-import {ManutodistformComponent} from '../manutodistform/manutodistform.component';
+import { ManutodistformComponent} from '../manutodistform/manutodistform.component';
 import { ManubyDistFormComponent } from '../manuby-dist-form/manuby-dist-form.component';
 import { ExportProductsToPharmacistComponent } from '../export-products-to-pharmacist/export-products-to-pharmacist.component';
 import { BuyProductsFromPharmacistComponent } from '../buy-products-from-pharmacist/buy-products-from-pharmacist.component';
-
+import { TimelineComponent} from '../timeline/timeline.component';
 export const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "home", component: HomeComponent },
@@ -19,5 +19,6 @@ export const routes: Routes = [
   { path: "manubydist",component:ManubyDistFormComponent},
   { path: "exporttopharm", component:ExportProductsToPharmacistComponent},
   { path: "buyfrompharma", component:BuyProductsFromPharmacistComponent},
+  { path: "timeline" , component:TimelineComponent},
   { path: "", pathMatch: "full", redirectTo: "/home" }
 ];
