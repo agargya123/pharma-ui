10c40664a2d225ead53ae48e3e064704f45e8aeb
Madhav Pruthi <madhavpruthi.becse16@pec.edu.in>
2020-05-28 12:57:18 +0530

    Modifications in timeline comp

--- frontend/src/app/transactions/transactions.component.ts
+++ frontend/src/app/transactions/transactions.component.ts
@@ -1,38 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ParticipantService } from '../services/participant.service';
+import { Component, OnInit } from "@angular/core";
+import { ParticipantService } from "../services/participant.service";
 
 @Component({
-  selector: 'app-transactions',
-  templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+  selector: "app-transactions",
+  templateUrl: "./transactions.component.html",
+  styleUrls: ["./transactions.component.scss"],
 })
 export class TransactionsComponent implements OnInit {
   listOfTransactions: any;
-  errorMessage: string;  
+  errorMessage: string;
 
   constructor(private participantService: ParticipantService) {
-    
-    this.participantService
-      .getAllTransactions()
-      .subscribe(
-        (data) => {
-          this.listOfTransactions = data;
-        },
-        (error) => {
-          this.errorMessage = "Not Authorized to view this";
-        }
-      );
+    this.participantService.getAllTransactions().subscribe(
+      (data) => {
+        this.listOfTransactions = data;
+      },
+      (error) => {
+        this.errorMessage = "Not Authorized to view this";
+      }
+    );
   }
 
-  isLast(index):boolean {
-
+  isLast(index): boolean {
     console.log(index);
 
-    return (index != this.listOfTransactions.length);
+    return index != this.listOfTransactions.length;
   }
 
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
